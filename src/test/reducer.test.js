2a8a17ad51ae54fc3272a553b68ef1e8e2d9cf7f
vcordero07 <vcordero07@gmail.com>
2018-02-01 20:49:51 -0500

    wip-reducer test

--- src/test/reducer.test.js
+++ src/test/reducer.test.js
@@ -21,6 +21,57 @@ describe("restartGame", () => {
     expect(state.guesses).toEqual([]);
     expect(state.feedback).toEqual("Make your guess!");
     //expect(state.answer).toEqual(state.answer);not equal 11
-    expect(state.answer).toEqual(state.answer);
+    expect(state.answer).not.toEqual(11);
+    expect(state.answer).toBeLessThanOrEqual(100);
+    expect(state.answer).toBeGreaterThanOrEqual(1);
+  });
+});
+
+describe("makeGuess", () => {
+  it("should make a guess", () => {
+    let state = {
+      guesses: ["1", "23", "34"],
+      answer: 11,
+      feedback: "You are warm"
+    };
+    const guess = 91;
+    state = Reducer(state, makeGuess(guess));
+    expect(state.answer).toBeLessThanOrEqual(100);
+    expect(state.answer).toBeGreaterThanOrEqual(1);
+    expect(state.feedback).toEqual("You are cold");
+    expect(state.guesses.length).toEqual(4);
+  });
+
+  it("should return a you are warm guess feddback", () => {
+    let state = {
+      guesses: ["1", "23", "34"],
+      answer: 11,
+      feedback: "You are warm"
+    };
+    const guess = 22;
+    state = Reducer(state, makeGuess(guess));
+    expect(state.feedback).toEqual("You are warm");
+  });
+
+  it("should return a you are hot guess feddback", () => {
+    let state = {
+      guesses: ["1", "23", "34"],
+      answer: 11,
+      feedback: "You are warm"
+    };
+    const guess = 13;
+    state = Reducer(state, makeGuess(guess));
+    expect(state.feedback).toEqual("You are hot");
+  });
+
+  it("should return a you are got it guess feddback", () => {
+    let state = {
+      guesses: ["1", "23", "34"],
+      answer: 11,
+      feedback: "You are warm"
+    };
+    const guess = 11;
+    state = Reducer(state, makeGuess(guess));
+    expect(state.feedback).toEqual("You got it");
   });
 });
